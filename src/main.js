1e65f007e8cda7630838a17cbcb1f98c365f7e32
Stephanie Faber <eyesicedover@gmail.com>
2018-03-16 16:55:50 -0700

    add form for life expectancy but it doesn't work, getting no errors

--- src/main.js
+++ src/main.js
@@ -16,4 +16,21 @@ $(document).ready(function() {
     $(".jupiterAge").text(agesArray[3].toFixed(2));
     $("#result").fadeIn();
   });
+
+  $("#deathForm").submit(function(event) {
+    event.preventDefault();
+    let deathAge = $("input#userDeath").val();
+    deathAge = parseInt(deathAge);
+    let userAge = newCalculator.userAge;
+    let agesArray = newCalculator.convertAllPlanetAges(userAge);
+    let deathArray = newCalculator.convertAllPlanetAges(deathAge);
+
+    let ageDiff = newCalculator.differenceOfAges(agesArray, deathArray);
+    $(".mercuryYeats").text(ageDiff[0].toFixed(2));
+    $(".venusYears").text(ageDiff[1].toFixed(2));
+    $(".marsYears").text(ageDiff[2].toFixed(2));
+    $(".jupiterYears").text(ageDiff[3].toFixed(2));
+    $("#deathYears").fadeIn();
+
+  });
 });
